17eeeca6ce9f10fb202b58d198c720bbfe53d90e
ysk2014 <1181102772@qq.com>
2019-08-07 10:24:44 +0800

    change

--- leetcode/10.正则表达式匹配.js
+++ leetcode/10.正则表达式匹配.js
@@ -12,3 +12,18 @@ var isMatch = function(s, p) {
     let reg = new RegExp('^' + p + '$');
     return reg.test(s);
 };
+
+/**
+ * 回溯法
+ */
+// var isMatch = function(s, p) {
+//     if (!p) return !s;
+
+//     let first_match = Boolean(s) && ['.', s[0]].includes(p[0]);
+
+//     if (p.length >= 2 && p[1] === '*') {
+//         return isMatch(s, p.substr(2)) || (first_match && isMatch(s.substr(1), p));
+//     } else {
+//         return first_match && isMatch(s.substr(1), p.substr(1));
+//     }
+// };
